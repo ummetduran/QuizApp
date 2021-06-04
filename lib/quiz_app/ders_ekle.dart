394beab650f43fb21142b50e190358ff4ed9f964
ummetduran <ummetduran@outlook.com>
2021-06-04 22:51:02 +0300

    ders ekle fonksiyonuyla uğraşılıyor

--- lib/quiz_app/ders_ekle.dart
+++ lib/quiz_app/ders_ekle.dart
@@ -1,17 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:untitled1/quiz_app/teacher_home_page.dart';
 
 import 'backend/Ders.dart';
+import 'backend/Teacher.dart';
 
-
+FirebaseAuth _auth = FirebaseAuth.instance;
+final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
 class DersEkle extends StatefulWidget {
+
   @override
   _DersEkleState createState() => _DersEkleState();
 }
+ Ders ders;
 
 class _DersEkleState extends State<DersEkle> {
   final formKey = GlobalKey<FormState>();
-  Ders ders;
-
+  Teacher teacher = TeacherHomePage().teacher;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,9 +45,7 @@ class _DersEkleState extends State<DersEkle> {
                     //suffixStyle: TextStyle(color: Colors.white)
 
                   ),
-                  onChanged: (name){
-                    ders.name= name;
-                  },
+                  onSaved: (name) => ders = new Ders(name, teacher)
 
                 ),
                 Padding(
@@ -50,7 +54,9 @@ class _DersEkleState extends State<DersEkle> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       RaisedButton(onPressed: (){
-                        dersEkle();
+
+                          dersEkle();
+
                       },
                         child: Text("Kaydet"),
 
@@ -72,8 +78,22 @@ class _DersEkleState extends State<DersEkle> {
     );
   }
 
-  void dersEkle() {
+  Future dersEkle() async {
+    debugPrint(ders.name.toString());
+    if(formKey.currentState.validate()) {
+      formKey.currentState.save();
+      //var fireUser = _auth.currentUser;
+      //Map<String, dynamic> dersEkle = Map();
+      //dersEkle['dersler'] = ders.name;
+      List<String> list = new List<String>();
+      list.add("${ders.name}");
+      //Users _user = Users(int.parse(_newUser.uid), fullName, _newUser.email);
+      await _fireStore.collection("Users").doc("${ders.teacher.id}").update(
+          {'dersler': FieldValue.arrayUnion(list)});
+//    await _fireStore.collection("Users").doc("${ders.teacher.id}").set('dersler': ).then((value) => debugPrint("eklendi"));
+      // await _fireStore.collection("Users").doc("${_newUser.uid}").collection("dersler").add(dersEkle);
 
+    }
   }
 
 
